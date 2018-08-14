98baa73dfec92cd3d777a39f3df91b0070d189d9
Pedro Leite <pedromonteiroleite@gmail.com>
2018-08-14 18:04:38 -0300

    Add api tests

---
+++ PaymentGatewayAPI.Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using Moq;
+//using System.Web.Http;
+using System.Net.Http;
+using PaymentGatewayAPI.Controllers;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGatewayAPI.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Get()
+        {
+            var Controller = new ValuesController();
+            var Response = Controller.Get();
+            IEnumerable<string> Values = Assert.IsAssignableFrom<IEnumerable<string>>(Response.Value);
+            Assert.Equal(2,( (string[])Values).Length);
+        }
+    }
+}
